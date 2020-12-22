ab4d781a8416bf6189bd259510893af6ee69bd58
WillJacques <68091327+WillJacques@users.noreply.github.com>
2020-12-21 21:23:15 -0500

    newemployee

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -6,12 +6,12 @@ namespace REST_API.Models
     public partial class Employee
     {
         public long Id { get; set; }
-        public long? user_id { get; set; }
+        public long? admin_user_id { get; set; }
 
         public string first_name { get; set; }
         public string last_name { get; set; }
-        public string function { get; set; }
-        public string phone { get; set; }
+        public string title { get; set; }
+        public string phone_number { get; set; }
         public string email { get; set; }
 
         public DateTime? created_at { get; set; }
