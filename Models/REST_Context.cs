238adfb78d2e600d6ebe5c20018c67d0f0cc5d1b
WillJacques <68091327+WillJacques@users.noreply.github.com>
2020-12-21 20:43:02 -0500

    newRESTAPI

--- Models/REST_Context.cs
+++ Models/REST_Context.cs
@@ -10,5 +10,16 @@ namespace REST_API.Models
         }
 
         public DbSet<intervention> interventions { get; set; }
+        public DbSet<User> users { get; set; }
+        public DbSet<Column> columns { get; set; }
+        public DbSet<Elevator> elevators { get; set; }
+        public DbSet<Building> buildings { get; set; }
+        public DbSet<Lead> leads { get; set; }
+        public DbSet<Battery> batteries { get; set; }
+        public DbSet<Address> addresses { get; set; }
+        public DbSet<BuildingDetail> building_details { get; set; }
+        public DbSet<Customer> customers { get; set; }
+        public DbSet<Employee> employees { get; set; }
+        public DbSet<Quote> quotes { get; set; }
     }
 }
